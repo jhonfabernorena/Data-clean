89ff8983f707e20d88d8584b67cf0ddbbdd39510
HernanRPereira <hernxnpereyra@gmail.com>
2024-07-29 10:10:51 -0500

    feat: add Swagger documentation setup
    - Added Swagger documentation configuration using .
    - Set up basic info with title 'Data Clean', description 'Team1 - Data Clean', and version '1.0'.
    - Added endpoint setup at '/api'.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Data Clean')
+    .setDescription('Team1 - Data Clean')
+    .setVersion('1.0')
+    .addTag('DataClean')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
