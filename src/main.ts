b266d81973102755e379bf3032c9da8d5816af4f
HernanRPereira <hernxnpereyra@gmail.com>
2024-07-29 11:51:32 -0500

    unit test

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,29 @@
-
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-
-
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ConfigService } from "@nestjs/config";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Configuración de variables de entorno
   const configService = app.get(ConfigService);
-  const port = configService.get<number>('PORT') || 3000;
+  const port = configService.get<number>("PORT") || 3000;
 
   // Configuración de CORS
   app.enableCors();
 
   // Configuración de Swagger
   const config = new DocumentBuilder()
-    .setTitle('API Key Management')
-    .setDescription('API for managing API keys')
-    .setVersion('1.0')
+    .setTitle("API Key Management")
+    .setDescription("API for managing API keys")
+    .setVersion("1.0")
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup("api", app, document);
 
   await app.listen(port);
   console.log(`Application is running on: http://localhost:3000/api  `);
-
 }
 
 bootstrap();
