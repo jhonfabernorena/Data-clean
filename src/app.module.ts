abe0c8f05c6b90d79db7496de366e5241cbf53d9
gelhg08 <angelicamhg31@gmail.com>
2024-07-29 13:16:41 -0500

    The function that receives files and uploads them to clourdinary was created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from "@nestjs/common";
+import { FileModule } from "./modules/file/file.module";
 
 @Module({
-  imports: [],
+  imports: [FileModule],
   controllers: [],
   providers: [],
 })
