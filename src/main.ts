d2daf8dbaaed3b17e4eaf109309830965ec9eecf
HernanRPereira <hernxnpereyra@gmail.com>
2024-07-30 12:42:27 -0500

    feat: Implement FileProcessingService with CSV processing
    - Added FileProcessingService to handle CSV file processing.
    - Included functionality for reading CSV from buffer and parsing data.
    - Implemented filtering of empty rows, deduplication, validation, and sorting.
    - Added method to generate a CSV buffer from processed records.
    - Integrated CloudinaryService for uploading processed CSV files.

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ConfigService } from "@nestjs/config";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
-import { cloudinaryConfig } from "./libs/cloudinary/cloudinary.config";
+import { cloudinaryConfig } from "./modules/cloudinary/cloudinary.config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
